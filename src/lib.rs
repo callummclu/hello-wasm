ee0ba4ca7b4cae6e35ca14c482cea9a0ab1941fd
Callum <callummcluskey100@gmail.com>
2022-08-28 16:40:35 +0100

    removed fluff

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
 use wasm_bindgen::prelude::*;
+use factorial::Factorial;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("should have a body on document");
+    let val = document.create_element("p")?;
+
+    let string_val = "Hello from rust!";
+
+    val.set_text_content(Some(string_val));
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str){
-    alert(&format!("Hello, {}", name));
-}
